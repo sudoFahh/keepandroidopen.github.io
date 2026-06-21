bef1d83229ef3572f54dd184b30625d8ec182ee0
beigestack <m4.sh@tuta.io>
2026-06-21 20:32:31 +0530

    Update banner.js

--- public/banner.js
+++ public/banner.js
@@ -56,6 +56,7 @@
     vi:      "Android sẽ trở thành một hệ điều hành đóng",
     bg:      "Android ще стане заключена платформа след",
     be:      "Android \u0441\u0442\u0430\u043d\u0435 \u0437\u0430\u043a\u0440\u044b\u0442\u0430\u0439 \u043f\u043b\u0430\u0444\u0442\u043e\u0440\u043c\u0430\u0439 \u0020 \u0020",
+    hi:      "Android एक लॉक्ड-डाउन प्लेटफ़ॉर्म बन जाएगा",
   };
 
   // ── Parse query parameters from the script's own src URL ──────────────
